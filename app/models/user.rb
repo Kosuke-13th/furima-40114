eb813b869d787ac17d9079e63de8467fdf6c6b8c
Kosuke-13th <bardiel.13th.angel9@gmail.com>
2023-12-14 20:43:46 +0900

    ユーザー管理機能rubocop処理

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-validates :nickname,        presence: true
-validates :nickname,        presence: true
-validates :password,        presence: true,length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/ }
-validates :last_name,       presence: true
-validates :first_name,      presence: true
-validates :last_name_kana,  presence: true,format: { with: /\A[ァ-ヶー－]+\z/ }
-validates :first_name_kana, presence: true,format: { with: /\A[ァ-ヶー－]+\z/ }
-validates :birth_day,       presence: true
-
-
+  validates :nickname,        presence: true
+  validates :nickname,        presence: true
+  validates :password,        presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/ }
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_day,       presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    nickname              { Faker::Name.initials(number: 2)}
-    email                 { Faker::Internet.email}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.email }
     password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
     last_name             { person.last.kanji }
