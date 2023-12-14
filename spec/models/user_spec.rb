be1ed3b7e5f95d20bf444de1e4438e26a40c2a50
Kosuke-13th <bardiel.13th.angel9@gmail.com>
2023-12-14 21:23:27 +0900

    ユーザー管理機能バリデーション修正、テストコード追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -73,6 +73,13 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Password is invalid')
       end
 
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'aaaaaＡ'
+        @user.password_confirmation = 'aaaaaＡ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
+
       it 'last_nameが空では登録できない' do
         @user.last_name = ''
         @user.valid?
@@ -83,6 +90,18 @@ RSpec.describe User, type: :model do
         @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+
+      it '姓（全角）に半角文字が含まれていると登録できない' do
+        @user.last_name = 'abc'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Last name is invalid')
+      end
+
+      it '名（全角）に半角文字が含まれていると登録できない' do
+        @user.first_name = 'cdf'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name is invalid')
       end
 
       it 'last_name_kanaが空では登録できない' do
