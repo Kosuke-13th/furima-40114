4c349597ac6c37e6552761f8b62e0823a4a78731
Kosuke-13th <bardiel.13th.angel9@gmail.com>
2023-12-15 18:37:07 +0900

    バリデーション追加　金額制限

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   validates :shipping_fee_id,   presence: true, numericality: { other_than: 1 }
   validates :prefecture_id,     presence: true, numericality: { other_than: 1 }
   validates :shipping_date_id,  presence: true, numericality: { other_than: 1 }
-  validates :price,             presence: true
+  validates :price,             presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :user,              presence: true
 
 
