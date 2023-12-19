4cdede698fcbd6f7200ca8fde8628bc00006604a
Kosuke-13th <bardiel.13th.angel9@gmail.com>
2023-12-19 14:20:04 +0900

    コントローラーへメソッド作成、メソッド内の不要な記述削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: :edit
+  before_action :item_set, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -43,10 +41,13 @@ class ItemsController < ApplicationController
                                  :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def item_set
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
     @item = Item.find(params[:id])
-    return if user_signed_in? && current_user.id == @item.user_id
-
+    return if current_user.id == @item.user_id
     redirect_to action: :index
   end
 end
