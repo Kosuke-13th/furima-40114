a018628b05352f5d041f999def9295cab237add8
Kosuke-13th <bardiel.13th.angel9@gmail.com>
2023-12-20 20:44:18 +0900

    購入機能テストコード記述

--- app/models/address_form.rb
+++ app/models/address_form.rb
@@ -5,11 +5,12 @@ class AddressForm
 
   validates :user_id,           presence: true
   validates :item_id,           presence: true
-  validates :postal_code,       presence: true, format: {with: /\A[0-9]+\z/}
+  validates :postal_code,       presence: true, format: {with: /\A\d{3}[-]\d{4}\z/}
   validates :prefecture_id,     presence: true, numericality: { other_than: 1 }
   validates :city,              presence: true
   validates :block,             presence: true
-  validates :phone_number,      presence: true
+  validates :phone_number,      presence: true, format: {with: /\A\d{10,11}\z/}
+
 
   def save
     buy = Buy.create(user_id:user_id, item_id: item_id)
